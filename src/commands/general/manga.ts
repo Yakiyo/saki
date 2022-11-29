7bd3ab5b004bfa80eb22357b268d7e7b39aa84d2
Yakiyo <91675932+Yakiyo@users.noreply.github.com>
2022-11-29 22:44:44 +0600

    feat: manga

---
+++ src/commands/general/manga.ts
@@ -0,0 +1,11 @@
+import { SlashCommandBuilder } from "discord.js";
+import { Command } from "../../struct/types";
+
+export const command: Command = {
+    data: new SlashCommandBuilder()
+        .setName('manga')
+        .setDescription('Sends link to Gimai Seikatsu MangaDex page.'),
+    async execute(interaction) {
+        interaction.reply("Here's the link to the **MangaDex** page <https://mangadex.org/title/99daf7bc-3a3b-4fe8-b10d-951b32bfea64>.");
+    },
+}
