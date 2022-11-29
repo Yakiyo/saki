a8238eba85668bc178e61595a15066ab27b57cd7
Yakiyo <91675932+Yakiyo@users.noreply.github.com>
2022-11-29 22:44:56 +0600

    feat: web

---
+++ src/commands/general/web.ts
@@ -0,0 +1,11 @@
+import { SlashCommandBuilder } from "discord.js";
+import { Command } from "../../struct/types";
+
+export const command: Command = {
+    data: new SlashCommandBuilder()
+        .setName('web')
+        .setDescription('Sends link to the Gimai Seikatsu Fan English TL online website.'),
+    async execute(interaction) {
+        interaction.reply("Here's the link for the Light Novel EN TL on CClaw <https://cclawtranslations.home.blog/gimai-seikatsu-toc/>.");
+    },
+}
