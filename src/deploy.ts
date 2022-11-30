a94426fece9c4e5143d7386a12c96c7fc44cf551
Yakiyo <91675932+Yakiyo@users.noreply.github.com>
2022-11-30 11:15:18 +0600

    refactor: command deploying

---
+++ src/deploy.ts
@@ -0,0 +1,4 @@
+import { CommandHandler } from "./struct/commandHandler";
+import "dotenv/config";
+
+new CommandHandler().registerInteractions();
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { Client as DiscordClient, GatewayIntentBits as Intents, ActivityType } from 'discord.js';
-import "dotenv/config";
 import { CommandHandler } from "./struct/commandHandler";
 import { EventHandler } from "./struct/eventHandler";
 import type { Client } from './struct/types';
+import "dotenv/config";
+
 
 const client = new DiscordClient({
     intents: [Intents.Guilds],
@@ -19,7 +20,7 @@ client.commandHandler = new CommandHandler();
 new EventHandler(client);
 
 if (process.env.DEPLOY === 'TRUE') {
-    client.commandHandler.registerInteractions();
+    require("./deploy");
 }
 
 client.login(process.env.DISCORD_TOKEN);
