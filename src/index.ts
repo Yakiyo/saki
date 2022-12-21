ce5b9d745d724e45ba1b4ff2e8585ffa6672a9b7
Yakiyo <yakiyosama5855@gmail.com>
2022-12-21 07:36:37 +0000

    feat: init distube

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@ import { Client as DiscordClient, GatewayIntentBits as Intents, ActivityType } f
 import { CommandHandler } from "./struct/commandHandler";
 import { EventHandler } from "./struct/eventHandler";
 import type { Client } from './struct/types';
+import { DisTube } from 'distube';
+import { log } from './util';
 import "dotenv/config";
-
 
 const client = new DiscordClient({
     intents: [
@@ -26,10 +27,24 @@ if (process.env.DEPLOY === 'TRUE') {
     require("./deploy");
 }
 
+const distube = new DisTube(client, {
+    searchSongs: 5,
+    searchCooldown: 30,
+    leaveOnEmpty: false,
+	leaveOnFinish: false,
+	leaveOnStop: false,
+    joinNewVoiceChannel: true
+});
+
+client.on('error', log);
+distube.on('error', log);
+
 declare global {
     var client: Client;
+    var distube: DisTube;
 }
 
 global.client = client;
+global.distube = distube;
 
 client.login(process.env.DISCORD_TOKEN);
