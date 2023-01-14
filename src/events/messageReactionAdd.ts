83b0b392795797c935552224597182666826af6e
Yakiyo <yakiyosama5855@gmail.com>
2023-01-14 14:50:16 +0000

    feat: spotlight module

--- src/events/messageReactionAdd.ts
+++ src/events/messageReactionAdd.ts
@@ -2,6 +2,7 @@ import type { Event } from '../struct/types';
 import { Events, MessageReaction, User, type Client } from 'discord.js';
 import { log } from '../util';
 import { reactionRole } from '../modules/reactionrole';
+import { spotlight } from '../modules/spotlight';
 
 export const event: Event = {
 	name: Events.MessageReactionAdd,
@@ -17,18 +18,14 @@ export const event: Event = {
 			}
 		}
 
-		const { prisma } = global;
+		const mod = await prisma.modules.findUnique({
+			where: {
+				id: 1,
+			},
+		});
 
-		const isRREnabled = await prisma.modules
-			.findUnique({
-				where: {
-					id: 1,
-				},
-			})
-			.then((m) => m?.Reaction_Roles);
+		if (mod?.Reaction_Roles) reactionRole(reaction, user);
 
-		if (isRREnabled) {
-			reactionRole(reaction, user);
-		}
+		if (mod?.Starboard) spotlight(reaction);
 	},
 };
---
+++ src/modules/spotlight.ts
@@ -0,0 +1,73 @@
+import type { APIEmbed, GuildTextBasedChannel, MessageReaction } from 'discord.js';
+import config from '../config';
+import { log } from '../util';
+
+const MIN_REACTIONS = 2;
+
+export async function spotlight(reaction: MessageReaction) {
+	if (!reaction.message.guild) return;
+	const reactions = reaction.message.reactions.cache;
+
+	const users = new Set<string>();
+	for (const r of reactions.values()) {
+		r.users.cache.filter((u) => !u.bot).forEach((e) => users.add(e.id));
+	}
+
+	if (Array.from(users).length < MIN_REACTIONS) return;
+
+	const prev = await prisma.spotlights.findFirst({
+		where: {
+			source: reaction.message.id,
+			channel: reaction.message.channelId,
+		},
+	});
+
+	if (prev) return; // A spotlight entry for that message already exists, so we don't bother with it
+
+	const channel = (await client.channels.fetch(config.channels.spotlight).catch((e) => {
+		log(e);
+		return null;
+	})) as GuildTextBasedChannel | null;
+	if (!channel) return;
+
+	const embed: APIEmbed = {
+		color: 16762880,
+		author: {
+			name: reaction.message.member?.user.username as string,
+			icon_url: reaction.message.member?.user.displayAvatarURL({ size: 1024 }),
+		},
+		timestamp: new Date().toISOString(),
+		footer: {
+			text: '⭐ Starred',
+		},
+		description: reaction.message.content as string | undefined,
+		fields: [
+			{
+				name: 'Source',
+				value: `[Jump Link](${reaction.message.url})`,
+			},
+		],
+	};
+	if (reaction.message.attachments.size) {
+		embed.image = {
+			url: reaction.message.attachments.first()?.url as string,
+		};
+	}
+	const message = await channel
+		.send({
+			content: `<#${reaction.message.channelId}>`,
+			embeds: [embed],
+		})
+		.catch(log);
+	if (!message) return;
+	await prisma.spotlights
+		.create({
+			data: {
+				message: message.id,
+				source: reaction.message.id,
+				channel: reaction.message.channelId,
+			},
+		})
+		.then(console.log);
+	return;
+}
