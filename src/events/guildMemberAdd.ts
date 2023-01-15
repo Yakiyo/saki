de21f72a9ba47a9c335b98e2aa7dc85b12f729f0
Yakiyo <yakiyosama5855@gmail.com>
2023-01-15 05:37:19 +0000

    feat: guildmember add audit log

--- src/events/guildMemberAdd.ts
+++ src/events/guildMemberAdd.ts
@@ -1,5 +1,5 @@
 import type { Event } from '../struct/types';
-import { Events, type GuildMember, type Client } from 'discord.js';
+import { Events, type GuildMember, type Client, GuildTextBasedChannel } from 'discord.js';
 import config from '../config';
 import { log } from '../util';
 import { welcome } from '../modules/welcome';
@@ -25,5 +25,31 @@ export const event: Event = {
 		if (!member.user.bot && isEnabled === true) {
 			welcome(member, client);
 		}
+		const channel = await client.channels.fetch(config.channels.activity_log).catch((e) => {
+			log(e);
+			return null;
+		});
+		if (!channel) return;
+		(channel as GuildTextBasedChannel).send({
+			embeds: [
+				{
+					author: {
+						name: 'Member Joined',
+						icon_url: member.user.displayAvatarURL(),
+					},
+					thumbnail: {
+						url: member.user.displayAvatarURL(),
+					},
+					color: 4437377,
+					description: `<@${member.id}> ${member.user.tag}`,
+					fields: [
+						{
+							name: 'Account Age',
+							value: `<t:${Math.ceil(member.user.createdAt.getTime() / 1000)}:R>`,
+						},
+					],
+				},
+			],
+		});
 	},
 };
