3e6ecf740fdb64137b76e48d5be9f096240b7dad
Yakiyo <yakiyosama5855@gmail.com>
2023-01-26 05:03:17 +0000

    feat: better ping command

--- src/commands/member/ping.ts
+++ src/commands/member/ping.ts
@@ -1,19 +1,31 @@
 import { SlashCommandBuilder } from 'discord.js';
 import { Command } from '../../struct/types';
+import { dateTimestamp } from '../../util';
 
 export const command: Command = {
 	data: new SlashCommandBuilder().setName('ping').setDescription('Replies with Pong!'),
 	async execute(interaction) {
-		const sent = await interaction.reply({
-			content: 'Pinging...',
+		const sent = await interaction.deferReply({
 			fetchReply: true,
 		});
-		await interaction.editReply(
-			`:ping_pong: Pong!\nUptime: ${Math.round(
-				interaction.client.uptime / 60000,
-			)} minutes\nWebsocket heartbeat: ${interaction.client.ws.ping}ms.\nRountrip Latency: ${
-				sent.createdTimestamp - interaction.createdTimestamp
-			}ms`,
-		);
+		// await interaction.editReply(
+		// 	`:ping_pong: Pong!\nUptime: ${Math.round(
+		// 		interaction.client.uptime / 60000,
+		// 	)} minutes\nWebsocket heartbeat: ${interaction.client.ws.ping}ms.\nRountrip Latency: ${
+		// 		sent.createdTimestamp - interaction.createdTimestamp
+		// 	}ms`,
+		// );
+		await interaction.editReply({
+			embeds: [
+				{
+					color: 16102651,
+					title: ':ping_pong: Pong!',
+					description:
+						`\n**Online since:** ${dateTimestamp(interaction.client.readyAt)}` +
+						`\n**Websocket heartbeat:** ${interaction.client.ws.ping}ms` +
+						`\n**Roundtrip Latency:** ${sent.createdTimestamp - interaction.createdTimestamp}ms`,
+				},
+			],
+		});
 	},
 };
