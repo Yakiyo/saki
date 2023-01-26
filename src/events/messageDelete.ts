ade6dcf8351af413fb53d27a7b82b72cc79bea38
Yakiyo <yakiyosama5855@gmail.com>
2023-01-26 04:44:21 +0000

    feat: add attachment names on delete event

--- src/events/messageDelete.ts
+++ src/events/messageDelete.ts
@@ -6,12 +6,13 @@ export const event: Event = {
 	name: Events.MessageDelete,
 	once: false,
 	async handle(message: Message) {
-		if (!message.guild) return;
+		if (!message.inGuild() || message.interaction) return;
 		const embed: APIEmbed = {
 			author: {
 				name: message.author.tag,
 				icon_url: message.author.displayAvatarURL() as string | undefined,
 			},
+			fields: [],
 			color: 16729871,
 			description: `**Message sent by <@${message.author.id}> deleted in <#${message.channelId}>**\n${message.content}`,
 			timestamp: new Date().toISOString(),
@@ -19,6 +20,16 @@ export const event: Event = {
 				text: `ID: ${message.author.id}`,
 			},
 		};
+		const attachments = [];
+		for (const [_, attachment] of message.attachments) {
+			attachments.push(attachment.name);
+		}
+		if (attachments.length) {
+			embed.fields?.push({
+				name: 'Attachments',
+				value: attachments.join(', '),
+			});
+		}
 		sendLog(embed, LogDestination.activity);
 	},
 };
