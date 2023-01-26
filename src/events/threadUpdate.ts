133fb1a9e8ccae3a07309a34ff973bfe29cf2b8e
Yakiyo <yakiyosama5855@gmail.com>
2023-01-26 04:27:25 +0000

    fix: dont unarchive non-modmail threads

--- src/events/threadUpdate.ts
+++ src/events/threadUpdate.ts
@@ -8,8 +8,16 @@ export const event: Event = {
 	async handle(oldThread: ThreadChannel, newThread: ThreadChannel) {
 		if (newThread.parentId !== config.channels.modmail) return;
 
-		// If a modmail thread gets auto archived, unarchive it.
-		if (!oldThread.archived && newThread.archived) {
+		const isOpen = await prisma.modmail
+			.findUnique({
+				where: {
+					threadId: newThread.id,
+				},
+			})
+			.then((m) => m?.isOpen);
+		// If a an open modmail on that thread exists, and it gets auto
+		// archived, unarchive it.
+		if (!oldThread.archived && newThread.archived && isOpen) {
 			newThread.setArchived(false);
 		}
 	},
