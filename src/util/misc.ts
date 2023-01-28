6794626584d7780750f9580acdd8cb50abd7ad88
Yakiyo <91675932+Yakiyo@users.noreply.github.com>
2023-01-28 23:19:00 +0600

    feat: clean utility func

--- src/util/misc.ts
+++ src/util/misc.ts
@@ -1,3 +1,5 @@
+import { inspect } from 'util';
+
 /**
  * Shorten a string upto num length
  */
@@ -8,6 +10,31 @@ export function shorten(str: string, num = 1000): string | undefined {
 	} else {
 		return str;
 	}
+}
+
+/**
+ * Clean a provided string-ish object of any sort of secrets
+ * of the bot.
+ */
+export async function clean(text: any) {
+	if (text && text.constructor.name === 'Promise') {
+		text = await text;
+	}
+
+	if (typeof text !== 'string') {
+		text = inspect(text, { depth: 1 });
+	}
+
+	text = text
+		.replace(/`/g, '`' + String.fromCharCode(8203))
+		.replace(/@/g, '@' + String.fromCharCode(8203));
+
+	const envs = ['DISCORD_TOKEN', 'DATABASE_URL', 'YOUTUBE_API_KEY', 'TWITTER_BEARER_TOKEN'];
+	for (const env of envs) {
+		const tokenRegex = new RegExp(process.env[env] as string, 'g');
+		text = text.replace(tokenRegex, `[${env}]`);
+	}
+	return shorten(text, 1800);
 }
 
 /**
