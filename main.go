8c41396b07edb8894546449bee030d6a5b84ba18
Yakiyo <yakiyo1206@gmail.com>
2024-09-29 19:39:14 +0600

    feat: initial setup of bot

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
 
-func main()  {
-	fmt.Println("Hello")
+	"github.com/bwmarrin/discordgo"
+	_ "github.com/joho/godotenv/autoload"
+)
+
+func main() {
+
+	token := os.Getenv("TOKEN")
+	if token == "" {
+		fmt.Println("Missing required env value TOKEN")
+		return
+	}
+
+	client, _ := discordgo.New("Bot " + token)
+
+	if err := client.Open(); err != nil {
+		fmt.Println("Failed to open connection", err)
+		return
+	}
+
+	defer client.Close()
+	fmt.Println("Starting bot...")
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+	<-stop
+	fmt.Println("Graceful shutdown")
 }
